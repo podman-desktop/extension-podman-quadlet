a7c36fab106efb1a059559de4b4812eb9c027db1
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-12-06 14:10:32 +0100

    fix: linter 🔨
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/monaco-editor/monaco.ts
+++ packages/frontend/src/lib/monaco-editor/monaco.ts
@@ -2,7 +2,7 @@ import * as monaco from 'monaco-editor';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 
 self.MonacoEnvironment = {
-  getWorker(_: any, label: string) {
+  getWorker(_: any) {
     return new editorWorker();
   },
 };
