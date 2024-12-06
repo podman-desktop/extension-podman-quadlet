08affe3521e669e5f68d741bf6393a86bff53f8b
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-12-06 16:38:09 +0100

    feat: monaco editor support dark mode (#20)
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/monaco-editor/MonacoEditor.svelte
+++ packages/frontend/src/lib/monaco-editor/MonacoEditor.svelte
@@ -17,13 +17,49 @@ let { content = $bindable(), language, readOnly = false, class: className, ...re
 let editor: Monaco.editor.IStandaloneCodeEditor;
 let editorContainer: HTMLElement;
 
+function getTerminalBg(): string {
+  const app = document.getElementById('app');
+  if (!app) throw new Error('cannot found app element');
+  const style = window.getComputedStyle(app);
+
+  let color = style.getPropertyValue('--pd-terminal-background').trim();
+
+  // convert to 6 char RGB value since some things don't support 3 char format
+  if (color?.length < 6) {
+    color = color
+      .split('')
+      .map(c => {
+        return c === '#' ? c : c + c;
+      })
+      .join('');
+  }
+  return color;
+}
+
 onMount(async () => {
+  const terminalBg = getTerminalBg();
+  const isDarkTheme: boolean = terminalBg === '#000000';
+
   import('monaco-editor/esm/vs/editor/editor.api').then(monaco => {
+    // define custom theme
+    monaco.editor.defineTheme('podmanDesktopTheme', {
+      base: isDarkTheme ? 'vs-dark' : 'vs',
+      inherit: true,
+      rules: [{ token: 'custom-color', background: terminalBg }],
+      colors: {
+        'editor.background': terminalBg,
+        // make the --vscode-focusBorder transparent
+        focusBorder: '#00000000',
+      },
+    });
+
     editor = monaco.editor.create(editorContainer, {
       value: content,
       language: language,
       automaticLayout: true,
+      scrollBeyondLastLine: false,
       readOnly: readOnly,
+      theme: 'podmanDesktopTheme',
     });
 
     editor.onDidChangeModelContent(() => {
