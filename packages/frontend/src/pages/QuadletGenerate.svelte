1e8b1337e048645d6e643774da745f4c0ae39310
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-04-14 10:26:41 +0200

    feat(podlet-js): replace podlet cli with internal solution (#486)
    * feat(podlet-js): replace podlet cli with internal solution
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix(e2e): ensure quadlet name is properly set
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix(e2e): linter
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/pages/QuadletGenerate.svelte
+++ packages/frontend/src/pages/QuadletGenerate.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-import { FormPage, EmptyScreen } from '@podman-desktop/ui-svelte';
+import { FormPage } from '@podman-desktop/ui-svelte';
 import Fa from 'svelte-fa';
-import { onMount } from 'svelte';
-import { podletAPI } from '/@/api/client';
 import ProgressBar from '/@/lib/progress/ProgressBar.svelte';
-import { faWarning } from '@fortawesome/free-solid-svg-icons/faWarning';
-import PodletInstall from '/@/lib/buttons/PodletInstall.svelte';
 import { router } from 'tinro';
 import QuadletGenerateForm from '/@/lib/forms/quadlet/QuadletGenerateForm.svelte';
 import { faCode } from '@fortawesome/free-solid-svg-icons/faCode';
@@ -15,19 +11,10 @@ import type { QuadletGenerateFormProps } from '/@/lib/forms/quadlet/quadlet-util
 let props: QuadletGenerateFormProps = $props();
 
 let loading: boolean = $state(false);
-let podletInstalled: boolean | undefined = $state(undefined);
-
-async function checkPodletInstallation(): Promise<void> {
-  podletInstalled = await podletAPI.isInstalled();
-}
 
 function close(): void {
   router.goto('/');
 }
-
-onMount(async () => {
-  await checkPodletInstallation();
-});
 </script>
 
 <FormPage
@@ -47,21 +34,12 @@ onMount(async () => {
       <!-- loading indicator -->
       <div class="h-0.5">
         <!-- avoid flickering -->
-        {#if loading || podletInstalled === undefined}
+        {#if loading}
           <ProgressBar class="w-full h-0.5" width="w-full" height="h-0.5" />
         {/if}
       </div>
 
-      {#if podletInstalled === false}
-        <EmptyScreen
-          icon={faWarning}
-          title="Podlet is not installed"
-          message="Podlet is used to generate Podman Quadlet files from a Podman command, compose file, or existing object">
-          <PodletInstall bind:loading={loading} onInstallCompleted={checkPodletInstallation} />
-        </EmptyScreen>
-      {:else if podletInstalled}
-        <QuadletGenerateForm close={close} {...props} bind:loading={loading} />
-      {/if}
+      <QuadletGenerateForm close={close} {...props} bind:loading={loading} />
     </div>
   </svelte:fragment>
 </FormPage>
