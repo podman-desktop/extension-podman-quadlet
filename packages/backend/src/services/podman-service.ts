f1d341cfbc7c2a0e2969eba6cec503e5ab30f908
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-05-07 17:18:05 +0200

    feat(podman): uses podman Native and SSH workers
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/backend/src/services/podman-service.ts
+++ packages/backend/src/services/podman-service.ts
@@ -1,31 +1,37 @@
 /**
  * @author axel7083
  */
-import type {
-  CancellationToken,
-  Logger,
-  ProviderContainerConnection,
-  RunResult,
-  Disposable,
-  RunError,
-} from '@podman-desktop/api';
-import { CancellationTokenSource } from '@podman-desktop/api';
+import type { ProviderContainerConnection, Disposable } from '@podman-desktop/api';
 import type { PodmanDependencies } from './podman-helper';
 import { PodmanHelper } from './podman-helper';
 import type { AsyncInit } from '../utils/async-init';
-import { dirname } from 'node:path/posix';
-import { writeFile, mkdir, readFile, rm } from 'node:fs/promises';
-import { homedir } from 'node:os';
-import { isRunError } from '../utils/run-error';
+import type { PodmanWorker } from '../utils/worker/podman-worker';
+import { PodmanNativeWorker } from '../utils/worker/podman-native-worker';
+import { readFile } from 'node:fs/promises';
+import { PodmanSSHWorker } from '../utils/worker/podman-ssh-worker';
+import type { PodmanConnection } from '../models/podman-connection';
 
 export class PodmanService extends PodmanHelper implements Disposable, AsyncInit {
   #extensionsEventDisposable: Disposable | undefined;
-  #execTimeout: number;
+  #pools: Map<string, PodmanWorker>;
+  #connections: Map<string, PodmanConnection>;
 
   constructor(dependencies: PodmanDependencies) {
     super(dependencies);
+    // create a Pool of Podman Workers
+    this.#pools = new Map();
+    // create a map to store the connections
+    this.#connections = new Map();
+  }
 
-    this.#execTimeout = 10_000;
+  /**
+   * Let's have a function to compute the key given a {@link ProviderContainerConnection}
+   * for the #pools of workers
+   * @param connection
+   * @protected
+   */
+  protected getKey(connection: ProviderContainerConnection): string {
+    return `${connection.providerId}:${connection.connection.name}`;
   }
 
   async init(): Promise<void> {
@@ -33,236 +39,15 @@ export class PodmanService extends PodmanHelper implements Disposable, AsyncInit
     this.#extensionsEventDisposable = this.dependencies.extensions.onDidChange(() => {
       this.resetPodmanExtensionApiCache(); // reset podman cache
     });
+    // listen for any providers update
+    this.dependencies.providers.event(this.onProviderUpdate.bind(this));
+    // update the collection of connections
+    return this.collectConnections();
   }
 
-  /**
-   * Feel very hacky
-   * @param connection
-   * @param path
-   */
-  async readTextFile(connection: ProviderContainerConnection, path: string): Promise<string> {
-    // linux native special case
-    if (this.isLinux && connection.connection.vmType === undefined) {
-      console.debug('[PodmanService] native connection using node:fs');
-      return readFile(path, { encoding: 'utf8' });
-    }
-
-    const result = await this.executeWrapper({
-      connection: connection,
-      args: [],
-      command: `cat "${path}"`,
-    });
-    return result.stdout;
-  }
-
-  /**
-   * Feel very hacky
-   * @param connection
-   * @param destination
-   * @param content
-   */
-  async writeTextFile(connection: ProviderContainerConnection, destination: string, content: string): Promise<void> {
-    // linux native special case
-    if (this.isLinux && connection.connection.vmType === undefined) {
-      console.debug('[PodmanService] native connection using node:fs');
-      // 1. resolve homedir
-      const resolved = destination.replace('~', homedir());
-      // 2. mkdir parent directory
-      await mkdir(dirname(resolved), { recursive: true });
-      // 3. write file
-      await writeFile(resolved, content, { encoding: 'utf8' });
-      return;
-    }
-
-    // mkdir
-    await this.executeWrapper({
-      connection: connection,
-      args: ['-p', dirname(destination)],
-      command: `mkdir`,
-    });
-
-    // write
-    await this.executeWrapper({
-      connection: connection,
-      args: [`"${content}"`, '>', destination],
-      command: `echo`,
-    });
-  }
-
-  /**
-   * remove a given file in the podman machine
-   * @dangerous
-   * @param connection
-   * @param path
-   */
-  async rmFile(connection: ProviderContainerConnection, path: string): Promise<void> {
-    // linux native special case
-    if (this.isLinux && connection.connection.vmType === undefined) {
-      console.debug('[PodmanService] native connection using node:fs');
-      return rm(path, { recursive: false, force: false });
-    }
-
-    await this.executeWrapper({
-      connection: connection,
-      args: [path],
-      command: `rm`,
-    });
-  }
-
-  /**
-   * This method should not be called directly, use {@link executeWrapper}
-   * @param options
-   * @private
-   */
-  private execute(options: {
-    connection: ProviderContainerConnection;
-    command: string;
-    args?: string[];
-    logger?: Logger;
-    env?: Record<string, string>;
-    token: CancellationToken;
-  }): Promise<RunResult> {
-    const { connection, command, args = [] } = options;
-
-    if (this.isLinux && connection.connection.vmType === undefined) {
-      console.warn('[PodmanService] provider do not have a VMType, considering native podman linux.');
-      console.debug(`[PodmanService] command ${command} args ${args}`);
-      return this.exec(command, args, {
-        logger: options.logger,
-        env: options.env,
-        token: options.token,
-      });
-    }
-
-    // throw an error if vmType is undefined on non-linux platform
-    if (connection.connection.vmType === undefined) {
-      throw new Error(
-        `connection ${options.connection.connection.name} of provider ${connection.providerId} vmType is undefined: not supported on non linux platform`,
-      );
-    }
-
-    const sshCommand = ['machine', 'ssh', options.connection.connection.name, `${command} ${args.join(' ')}`];
-    console.debug('[PodmanService] ssh command', sshCommand);
-
-    return this.podman.exec(sshCommand, {
-      connection: connection,
-      logger: options.logger,
-      env: options.env,
-      token: options.token,
-    });
-  }
-
-  /**
-   * This method execute a given command in the podman machine
-   * @remarks if the podman connection is native, the command will be executed on the host.
-   * @remarks if no CancellationToken is provided one will be created with a default timeout
-   * @remarks use internal {@link execute}
-   * @dangerous
-   * @protected
-   */
-  protected async executeWrapper(options: {
-    connection: ProviderContainerConnection;
-    command: string;
-    args?: string[];
-    logger?: Logger;
-    env?: Record<string, string>;
-    token?: CancellationToken;
-  }): Promise<RunResult> {
-    // we should always have a default timeout if no CancellationToken is provided
-    let token: CancellationToken;
-    let timeoutId: ReturnType<typeof setTimeout> | undefined;
-    let source: CancellationTokenSource | undefined;
-    if (options.token) {
-      token = options.token;
-    } else {
-      source = new CancellationTokenSource();
-      token = source.token;
-
-      timeoutId = setTimeout(() => {
-        source?.cancel();
-        source?.dispose();
-      }, this.#execTimeout);
-    }
-
-    let result: RunResult;
-    try {
-      result = await this.execute({ ...options, token: token });
-    } finally {
-      clearTimeout(timeoutId);
-      source?.dispose();
-    }
-    return result;
-  }
-
-  /**
-   * @privateRemarks We do not expose {@link executeWrapper} for security purpose, so we must expose the quadlet exec
-   * @param options
-   */
-  quadletExec(options: {
-    connection: ProviderContainerConnection;
-    args: string[];
-    logger?: Logger;
-    token?: CancellationToken;
-    env?: Record<string, string>;
-  }): Promise<RunResult | RunError> {
-    return this.executeWrapper({
-      ...options,
-      command: '/usr/libexec/podman/quadlet',
-    }).catch((err: unknown) => {
-      // check err is an RunError
-      if (isRunError(err)) return err;
-      throw err;
-    });
-  }
-
-  /**
-   * systemctl has a weird specificity to change the return code depending on the status.
-   * | value   | Description in LSB                             | Use in systemd                     |
-   * | :------ | :--------------------------------------------  | :--------------------------------- |
-   * | 0       | program is running or service is OK            | unit is active                     |
-   * | 1       | program is dead and /var/run pid file exists   | unit not failed (used by is-failed)|
-   * | 2       | program is dead and /var/lock lock file exists | unused                             |
-   * | 3       | program is not running                         | unit is not active                 |
-   * | 4       | program or service status is unknown           | no such unit                       |
-   *
-   * ref {@link https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#Exit%20status}
-   *
-   * @privateRemarks We do not expose {@link executeWrapper} for security purpose, so we must expose the systemctl exec
-   *
-   * @dangerous
-   * @param options
-   */
-  async systemctlExec(options: {
-    connection: ProviderContainerConnection;
-    args: string[];
-    logger?: Logger;
-    token?: CancellationToken;
-    env?: Record<string, string>;
-  }): Promise<RunResult | RunError> {
-    return this.executeWrapper({
-      ...options,
-      command: 'systemctl',
-    }).catch((err: unknown) => {
-      // check err is an RunError
-      if (isRunError(err)) return err;
-      throw err;
-    });
-  }
-
-  /**
-   * @param options
-   */
-  async journalctlExec(options: {
-    connection: ProviderContainerConnection;
-    args: string[];
-    logger?: Logger;
-    token?: CancellationToken;
-    env?: Record<string, string>;
-  }): Promise<RunResult> {
-    return this.executeWrapper({
-      ...options,
-      command: 'journalctl',
-    });
+  protected onProviderUpdate(): void {
+    // update the podman remote connections info
+    this.collectConnections().catch(console.error);
   }
 
   /**
@@ -282,7 +67,84 @@ export class PodmanService extends PodmanHelper implements Disposable, AsyncInit
     return result.stdout.trim() === 'true';
   }
 
+  protected async collectConnections(): Promise<void> {
+    const connections = await this.getPodmanConnections();
+    this.#connections = new Map<string, PodmanConnection>(connections.map(connection => [connection.Name, connection]));
+  }
+
+  public hasConnection(connection: ProviderContainerConnection): boolean {
+    return this.#connections?.has(connection.connection.name) ?? false;
+  }
+
+  public getConnection(connection: ProviderContainerConnection): PodmanConnection {
+    const remote = this.#connections?.get(connection.connection.name);
+    if (!remote) throw new Error(`could not get remote connection for connection ${connection.connection.name}`);
+    return remote;
+  }
+
+  /**
+   * To interact with a podman instance we use {@link PodmanWorker} class
+   * which has a series of functions (read, write, rm, exec etc.)
+   *
+   * The {@link PodmanWorker} in an abstract class, as depending on the instance would need
+   * specific logic.
+   *
+   * This function get a worker from the cache if we already have one, otherwise will instantiate
+   * a new worker for the given connection.
+   *
+   * @param connection
+   */
+  async getWorker(connection: ProviderContainerConnection): Promise<PodmanWorker> {
+    const key = this.getKey(connection);
+    const worker = this.#pools.get(key);
+    if (worker) return worker;
+
+    // detect podman linux native
+    if (this.isLinux && !this.hasConnection(connection)) {
+      const native = new PodmanNativeWorker(connection, this.dependencies.processApi);
+      // init
+      await native.init();
+      // cache
+      this.#pools.set(key, native);
+      return native;
+    }
+
+    // create PodmanSSHWorker
+    const { URI, Identity, IsMachine } = this.getConnection(connection);
+    if (!IsMachine) {
+      console.warn('[PodmanWorkers] detected remote connection: this is an experimental feature');
+    }
+
+    const url = new URL(URI);
+    if (!url) throw new Error('cannot parse URI from podman connection: null URI');
+    if (!Identity) throw new Error('remote connection without identity specified is not supported');
+
+    const privateKey = await readFile(Identity, { encoding: 'utf8' });
+
+    // create the SSH Worker
+    const sshWorker = new PodmanSSHWorker(connection, {
+      host: url.hostname,
+      port: parseInt(url.port),
+      username: url.username,
+      privateKey,
+    });
+
+    // init
+    await sshWorker.init();
+
+    // update the pools
+    this.#pools.set(key, sshWorker);
+    return sshWorker;
+  }
+
   dispose(): void {
     this.#extensionsEventDisposable?.dispose();
+
+    // dispose workers
+    this.#pools.forEach(pool => pool.dispose());
+    this.#pools.clear();
+
+    // cleanup connection map
+    this.#connections?.clear();
   }
 }
