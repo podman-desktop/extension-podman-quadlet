5c1aa27f17c4463b9cefe2732f7f38961a6dd784
axel7083 <42176370+axel7083@users.noreply.github.com>
2026-07-20 14:03:45 +0200

    fix: formatter
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- packages/frontend/src/lib/buttons/RadioButtons.svelte
+++ packages/frontend/src/lib/buttons/RadioButtons.svelte
@@ -33,12 +33,16 @@ function onclick(id: string): void {
         role="radio"
         aria-checked={selected}
         onclick={onclick.bind(undefined, option.id)}
-        class={['inline-block w-full h-full', {
-          'bg-(--pd-button-primary-bg) text-(--pd-button-primary-text)': selected && !disabled,
-          'text-[var(--pd-button-secondary-text)] hover:bg-[var(--pd-button-secondary-hover-bg)]': !selected && !disabled,
-          'bg-(--pd-button-disabled) text-(--pd-button-disabled-text) cursor-not-allowed': disabled,
-          'cursor-pointer': !disabled,
-        }]}
+        class={[
+          'inline-block w-full h-full',
+          {
+            'bg-(--pd-button-primary-bg) text-(--pd-button-primary-text)': selected && !disabled,
+            'text-[var(--pd-button-secondary-text)] hover:bg-[var(--pd-button-secondary-hover-bg)]':
+              !selected && !disabled,
+            'bg-(--pd-button-disabled) text-(--pd-button-disabled-text) cursor-not-allowed': disabled,
+            'cursor-pointer': !disabled,
+          },
+        ]}
         title={option.label}
         aria-label={option.label}
         aria-current="page">
